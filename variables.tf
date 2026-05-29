d8e03e22192351b4be68bdd61cbc85db4439923e
Sanjeev Singh <sanjeevsingh@Sanjeevs-Mini.lan>
2026-05-29 07:52:58 +0530

    Changed Naming convention, generic code

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "rgdetails" {
   description = "Map of Azure Resource Groups"
   type = map(object({
-    rg_name  = string
+    name  = string
     location = string
     managed_by  = optional(string)
     tags     = optional(map(string))
